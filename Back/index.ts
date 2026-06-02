737b22890e375f6dbd7ebf197cdc3eb161811e45
Fran Rodriguez <fran02_1996@hotmail.com>
2026-06-02 20:50:23 -0300

    feat: setup backend server and database connection

---
+++ Back/index.ts
@@ -0,0 +1,37 @@
+import { env } from "./config/env";
+import app from "./app";
+import { conexionDB } from "./config/db";
+import { logger } from "./config/logger";
+
+const PORT = env.PORT;
+
+
+// Testear conexión a base de datos y luego levantar servidor:
+
+async function main() {
+  try {
+    const connection = await conectionDB.getConnection();
+
+    logger.info("Conectado a la base de datos con exito");
+
+    // Devuelve la conexin al pool para que pueda reutilizarse
+    connection.release();
+
+    //Levantar el servidor:
+    app.listen(PORT, () => {
+      logger.info(`Servidor corriendo en puerto: ${PORT}`);
+    });
+
+  } catch (error) {
+
+    logger.error(
+      { error },
+      "Error al conectar con la base de datos"
+    );
+
+    process.exit(1); //indica si termino por algun fallo
+  }
+}
+
+
+main();
